91b8036c656ebddea170dfd849582c8183047e88
hensing1 <inbox@jhlehmann.de>
2026-03-23 23:57:58 +0100

    summarie

--- src/analyze.py
+++ src/analyze.py
@@ -1,14 +1,12 @@
 import src.db_connector as db_connector
 from src.sankey import SankeyGraph
 
-from dash import dcc, callback, Input, Output
+from dash import dcc, callback, Input, Output, html
 from dash.exceptions import PreventUpdate
 from plotly import graph_objects as go
 
 
-def make_sankey(transactions):
-    einnahmen = [t for t in transactions if t["Einnahme"] and not t["ignorieren"]]
-    ausgaben = [t for t in transactions if not t["Einnahme"] and not t["ignorieren"]]
+def make_sankey(einnahmen, ausgaben):
 
     g = SankeyGraph()
 
@@ -34,19 +32,42 @@ def make_sankey(transactions):
     )
 
 
+def make_summary(einnahmen, ausgaben):
+    sum_ein = sum([t["Betrag"] for t in einnahmen]) / 100
+    sum_aus = abs(sum([t["Betrag"] for t in ausgaben])) / 100
+    return [
+        html.P(f"Einnahmen: {sum_ein}€", style={"width": "400px"}),
+        html.P(f"{round(sum_ein - sum_aus, 2)}€",
+               style={"color": ("green" if sum_ein > sum_aus else "red")}),
+        html.P(f"Ausgaben: {sum_aus}€", style={"width": "400px"}),
+    ]
+
+
 @callback(
     Output("sankey_graph", "figure"),
+    Output("summary_div", "children"),
     Input("sankey_range", "start_date"),
     Input("sankey_range", "end_date"),
-    Input("save_button", "n_clicks")
+    Input("tabs", "value"),
+    Input("save_button", "n_clicks"),
 )
-def update_sankey(start_date, end_date, _):
-    if (start_date is None or end_date is None):
+def update_sankey(start_date, end_date, value, _):
+    if (start_date is None or end_date is None or value != "tab-1"):
         raise PreventUpdate
+
     transactions = db_connector.select_transactions_as_view(start_date, end_date)
-    sankey = make_sankey(transactions)
-    return go.Figure(sankey)
+
+    einnahmen = [t for t in transactions if t["Einnahme"] and not t["ignorieren"]]
+    ausgaben = [t for t in transactions if not t["Einnahme"] and not t["ignorieren"]]
+    sankey = make_sankey(einnahmen, ausgaben)
+    summary = make_summary(einnahmen, ausgaben)
+    return go.Figure(sankey), summary
 
 
 def create_analyzer():
-    return dcc.Graph(id="sankey_graph")
+    return html.Div([
+        html.Div(id="summary_div",
+                 style={"display": "flex", "justifyContent": "space-around",
+                        "textAlign": "center"}),
+        dcc.Graph(id="sankey_graph")
+    ])
